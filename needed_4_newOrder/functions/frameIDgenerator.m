4c0b3abe80a5481480a983da3e250882b6f46332
patecumbia <gemalvalk@gmail.com>
2022-10-18 01:34:28 -0500

    Delete needed_4_newOrder directory

--- needed_4_newOrder/functions/frameIDgenerator.m
+++
@@ -1,7 +0,0 @@
-%% 10.09.2020 function to genereate differnt IDs for each frame
-function frameID = frameIDgenerator(locEv)
-frameID = zeros(length(locEv),1);
-for ev = 1:length(locEv)
-    frameID(ev,1) =  str2double([num2str([locEv(ev).synID]), num2str([locEv(ev).frame])]);
-    
-end
